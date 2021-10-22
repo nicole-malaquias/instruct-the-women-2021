0363c7ceb5fd5832b6199c37070dae1955c50d0f
Nicole Malaquias <nicolemalaquias91@gmail.com>
2021-10-22 16:04:57 +0000

    feat validate is completed

--- api/serializers.py
+++ api/serializers.py
@@ -11,6 +11,42 @@ class PackageSerializer(serializers.ModelSerializer):
         extra_kwargs = {"version": {"required": False}}
 
     def validate(self, data):
+
+       
+
+        new_data = data.items()
+
+        name = [{name[0]:name[1]} for name in new_data if name[0] == 'name'][0]
+        version = [{version[0]:version[1]} for version in new_data if version[0] == 'version'][0]
+      
+        import json
+        import requests
+        query = name['name']
+
+        request = requests.get(f'https://pypi.org/pypi/{query}/json')
+
+        if request.status_code == 404 :
+            raise serializers.ValidationError()
+
+        response = json.loads(request.content)
+
+        if  version['version'] in response['releases']:
+            
+            return data
+
+        raise serializers.ValidationError()
+  
+        
+       
+
+
+
+
+
+
+
+
+
         # TODO
         # Validar o pacote, checar se ele existe na versão especificada.
         # Buscar a última versão caso ela não seja especificada pelo usuário.
